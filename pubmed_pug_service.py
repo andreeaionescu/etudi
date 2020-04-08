638bc897cc69bdbd45aafe7cdcfa286407249d37
Andreea Ionescu <amionescu15@gmail.com>
2020-04-09 00:50:31 +0100

    Pass output format as a variable

--- pubmed_pug_service.py
+++ pubmed_pug_service.py
@@ -10,7 +10,7 @@ def parse_webenv_and_querykey(xml_root):
     return xml_root.find('WebEnv').text, xml_root.find('QueryKey').text
 
 
-def retrieve_data_by_query(db, search_txt):
+def retrieve_data_by_query(db, search_txt, output_format):
     try:
         url_base = URL + f'esearch.fcgi?db={db}&term={search_txt}&reldate=1&datetype=edat&retmax=10&usehistory=y'
         response = requests.get(url_base)
@@ -26,7 +26,7 @@ def retrieve_data_by_query(db, search_txt):
 
             # include for ESearch-EFetch
             # assemble the efetch URL
-            efetch_url = url_base + f'efetch.fcgi?db={db}&query_key={querykey}&WebEnv={webenv}&rettype=abstract&retmode=json'
+            efetch_url = url_base + f'efetch.fcgi?db={db}&query_key={querykey}&WebEnv={webenv}&rettype=abstract&retmode={output_format}'
             # post the efetch URL
             data = requests.get(efetch_url)
             data.raise_for_status()
@@ -40,5 +40,5 @@ if __name__ == '__main__':
     db = 'pubmed'
     search_txt = 'aspirin'
     output_format = 'JSON'  # XML | CSV | PNG | TXT
-    aspirin_record = retrieve_data_by_query(db, search_txt)
+    aspirin_record = retrieve_data_by_query(db, search_txt, output_format)
     pprint(aspirin_record)  # returns an xml with UIDs to matching entries
