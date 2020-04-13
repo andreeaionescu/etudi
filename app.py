12459f45f1a5285570e9d5a2f9ccd1dfcc0eee55
Andreea Ionescu <amionescu15@gmail.com>
2020-04-13 15:40:18 +0100

    Render title results in the same html page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tornado.web import Application, RequestHandler
+from tornado.web import Application, HTTPError
 from tornado.ioloop import IOLoop
 from pubmed_service import EntrezConnection
 
@@ -6,18 +6,20 @@ from pubmed_service import EntrezConnection
 class ArticleHandler(EntrezConnection):
 
     def get(self):
-        self.render('templates/index.html')
+        self.render('templates/index.html', items=[])
 
     def post(self):
-        self.set_header("Content-Type", "text/plain")
-        message = self.get_body_argument("message")
+        message = self.get_body_argument('message')
         response = self.query(message)
-        self.write(response)
+        titles = [response[key]['title'] for key in response.keys()]
+        if not response:
+            raise HTTPError(404)
+        self.render('templates/index.html', items=titles)
 
 
 if __name__ == '__main__':
     app = Application([
-        (r"/", ArticleHandler)
+        (r'/', ArticleHandler)
     ])
     app.listen(8888)
     IOLoop.instance().start()
