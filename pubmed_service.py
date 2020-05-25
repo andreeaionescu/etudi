c29c92661e0b3bf412fbbb4cbcabc264fa4a0b6b
Andreea Ionescu <amionescu15@gmail.com>
2020-05-25 13:48:44 +0100

    Fixed copyright and keywordlist issues. Added handler for articlebyid fulltext. xml parser class to transform the full text from xml to dict.

--- pubmed_service.py
+++ pubmed_service.py
@@ -37,18 +37,22 @@ class EntrezConnection(RequestHandler):
     def entrez_link_pubmed_pmc(self, pubmed_id):
         return Entrez.read(Entrez.elink(dbfrom='pubmed', db='pmc', LinkName="pubmed_pmc", id=pubmed_id))
 
-    def entrez_fetch_full_text(self, pubmed_id):
+    def get_pmc_from_pubmed(self, pubmed_id):
         link_db = self.entrez_link_pubmed_pmc(pubmed_id)[0].get('LinkSetDb')
-        pmc_id = link_db[0].get('Link')[0].get('Id') if len(link_db) !=0 else None
+        return link_db[0].get('Link')[0].get('Id') if len(link_db) != 0 else None
+
+    def entrez_fetch_full_text(self, pubmed_id):
+        pmc_id = self.get_pmc_from_pubmed(pubmed_id)
         return Entrez.efetch(db='pmc', id=pmc_id).read() if pmc_id else None
 
     def query_full_text(self, pubmed_id):
+        pmc_id = self.get_pmc_from_pubmed(pubmed_id)
         entrez_fetch_full_text = self.entrez_fetch_full_text(pubmed_id)
         if entrez_fetch_full_text:
             root = ElementTree.XML(entrez_fetch_full_text)
-            return XmlDictConfig(root)
+            return {'pubmed_id': pubmed_id, 'pmc_id': pmc_id, 'full_text': XmlDictConfig(root).get('article', None)}
         else:
-            return {'pubmed_id': pubmed_id, 'full_text': None}
+            return {'pubmed_id': pubmed_id, 'pmc_id': pmc_id, 'full_text': None}
 
     def query(self, stmt):
         return display_basic_details(self.entrez_read(stmt))
--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+import datetime
+import json
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
 
 def display_title_abstract(response):
     abstracts = {}
@@ -11,3 +22,41 @@ def display_title_abstract(response):
             abstract = 'No abstract provided.'
         abstracts[pmid] = {'title': title, 'abstract': abstract}
     return abstracts
+
+
+def get_publication_details(journal_issue):
+    publication_details = ''
+    pub_date = journal_issue.get('PubDate')
+    if pub_date:
+        pub_date_day = pub_date.get('Day') + ' ' if pub_date.get('Day') else ''
+        pub_date_month = pub_date.get('Month') + ' ' if pub_date.get('Month') else ''
+        pub_date_year = pub_date.get('Year', '')
+        publication_details += f'{pub_date_day}{pub_date_month}{pub_date_year}'
+    return publication_details
+
+
+def display_basic_details(response):
+    details = {}
+    for article in response['PubmedArticle']:
+        pmid = article['MedlineCitation']['PMID']
+        title = article['MedlineCitation']['Article']['ArticleTitle']
+        copyright = article['MedlineCitation']['Article']['Abstract'].get('CopyrightInformation', '') if article['MedlineCitation']['Article'].get('Abstract') else None
+        keywords = article['MedlineCitation'].get('KeywordList', [])
+        authors = [author.get('ForeName', '') + ' ' + author.get('LastName', '') for author in article['MedlineCitation']['Article'].get('AuthorList', [])]
+        journal_name = article['MedlineCitation']['Article']['Journal']['Title']
+        publication_details = get_publication_details(article['MedlineCitation']['Article']['Journal']['JournalIssue'])  #TODO do we want more details on journal such as JournalIssue, pubdated etc.
+        abstract = ''.join([
+            abstract_text for abstract_text in article['MedlineCitation']['Article']['Abstract']['AbstractText']]) \
+            if article['MedlineCitation']['Article'].get('Abstract') \
+            else 'No abstract available.'
+        details[pmid] = {'Title': title,
+                         'Abstract': abstract,
+                         'Authors': authors,
+                         'Copyright': copyright,
+                         'Keywords': keywords,
+                         'Journal': {
+                             'PublicationDetails': publication_details,
+                             'Title': journal_name
+                            }
+                         }
+    return details
